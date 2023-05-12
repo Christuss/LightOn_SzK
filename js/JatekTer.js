dc04536aea23f72262c7abca09675e65957bfaa5
Christuss <szendefi.krisztofer@gmail.com>
2023-05-12 10:37:53 +0200

    kattintas kesz, szomszedok felkesz

--- js/JatekTer.js
+++ js/JatekTer.js
@@ -1,3 +1,5 @@
+import Lampa from "./Lampa.js";
+
 class Jatekter {
     #db;
     #allapotLista = [];
@@ -5,15 +7,35 @@ class Jatekter {
     #lepes;
 
     constructor () {
+        this.#meret = 3;
+        this.#db = this.#meret * this.#meret;
+        const MAINELEM = $("main");
+        for (let index = 0; index < this.#db; index++) {
+            let bool = Math.random() < 0.2
+            let a = new Lampa(index, bool, MAINELEM);
+            this.#setAllapotLista(bool);
+        }
 
+        $(window).on("kattintas", (event) => {
+            console.log(event.detail);
+            this.#szomszedokKeresese(event.detail);
+        });
     }
 
-    #setAllapotLista () {
-
+    #setAllapotLista (bool) {
+        this.#allapotLista.push(bool);
     }
 
     #szomszedokKeresese (id) {
-
+        let szomszedok = [] ;
+        switch (id) {
+            case 0:
+                this.#allapotLista[1] = false;
+                break;
+        
+            default:
+                break;
+        }
     }
 
     #init() {
@@ -21,7 +43,7 @@ class Jatekter {
     }
 
     #ellenorzes() {
-        
+
     }
 }
 
--- js/Lampa.js
+++ js/Lampa.js
@@ -4,18 +4,44 @@ class Lampa {
     #divElem;
 
     constructor(id, allapot, szuloElem) {
+        this.#id = id;
+        this.#allapot = allapot;
+        let txt;
+        if (allapot) {
+            txt = '<div class = "zold" id = "' + this.#id + '"></div>';
+        } else {
+            txt = '<div class = "piros" id = "' + this.#id + '"></div>';
+        }
+        szuloElem.append(txt);
+        this.#divElem = $("main div:last-child");
 
-    }
-
-    setAllapot() {
-
-    }
-
-    #szinBeallit() {
+        this.#divElem.on("click", (event) => {
+            this.#kattintasTrigger();
+            this.setAllapot();
+            this.#szinBeallit();
+        })
         
     }
 
-    #kattintasTrigger () {
+    setAllapot() {
+        if (this.#allapot) {
+            this.#allapot = false;
+        } else {
+            this.#allapot = true;
+        }
+    }
+
+    #szinBeallit() {
+        if (this.#divElem.attr("class") === "zold") {
+            this.#divElem.attr("class", "piros");
+        } else {
+            this.#divElem.attr("class", "zold");
+        }
+    }
+
+    #kattintasTrigger() {
+        const EVENT = new CustomEvent("kattintas", { detail: this.#id });
+        window.dispatchEvent(EVENT);
 
     }
 }
